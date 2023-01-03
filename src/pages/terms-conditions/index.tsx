7e9f6377d44c450e569e78dab56bf7fd86b9ce2d
André Torres <andretorresjava@gmail.com>
2023-01-03 15:59:05 -0500

    Build the terms and conditions page.

--- src/pages/terms-conditions/index.tsx
+++ src/pages/terms-conditions/index.tsx
@@ -1,13 +1,28 @@
 import classNames from 'classnames';
-import { Container } from '@gamiui/standard';
+import { Container, RichText, Spacer } from '@gamiui/standard';
 
 import { LayoutWrapper } from '../../common/layouts';
+import * as S from './styles';
 
 export default function termsConditions() {
 
   return (
-    <Container className={classNames('topics')}>
-      
+    <Container height='full' className={classNames('topics')}>
+      <S.ContentWrapper>
+        <RichText 
+          text='Términos y condiciones del servicio'
+          style={{
+            textTransform: 'uppercase',
+            textAlign: 'center',
+            textDecoration: 'underline',
+            marginBottom: '1rem'
+          }}
+        />
+        {/* <Spacer customSize='16px' direction='bottom' /> */}
+        <Container>
+          <RichText text='A continuación, podrán leer los términos y condiciones mediante los que usted podrá usar nuestro servicio de pedidos. Al acceder y utilizar nuestros servicios, usted acepta que ha leído, entiende y está de acuerdo con las condiciones descritas en esta sección "Términos y Condiciones”.' />
+        </Container>
+      </S.ContentWrapper>
     </Container>
   )
 }
---
+++ src/pages/terms-conditions/styles.tsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+import { Container } from '@gamiui/standard';
+
+export const ContentWrapper = styled(Container)`
+    height: 100%;
+    margin: auto;
+    padding: 15px 40px 15px 78px;
+    width: 90%;
+`;
